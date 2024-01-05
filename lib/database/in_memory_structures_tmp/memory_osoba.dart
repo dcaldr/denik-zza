6682afdbbdc163d4abac4bfa9410877b76e9d87b
Dan Caldr <cald01@vse.cz>
2024-01-05 16:37:48 +0100

    fix: add gender

--- lib/database/in_memory_structures_tmp/memory_osoba.dart
+++ lib/database/in_memory_structures_tmp/memory_osoba.dart
@@ -7,13 +7,14 @@ class MemoryOsoba {
   String prijmeni;
   DateTime? datumNarozeni;
   String? adresa;
-  String? telefonniCislo;
+  String? telefonniCislo; // rodiče
   String? zdravotniPojistovna;
   String? cisloPojisteni;
+  int? pohlavi; // 0 žena 1 muž
   late   int id; // may use -1 means not assigned
 
   MemoryOsoba(this.jmeno, this.prijmeni, this.datumNarozeni, this.adresa,
-      this.telefonniCislo, this.zdravotniPojistovna, this.cisloPojisteni);
+      this.telefonniCislo, this.zdravotniPojistovna, this.cisloPojisteni, this.pohlavi,);
   MemoryOsoba.basic(this.jmeno, this.prijmeni);
 
 
