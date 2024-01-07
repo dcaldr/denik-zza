d501d963add843f3e83909258eff05b3a04628c8
Ester Stankovska <stankovskaester@gmail.com>
2024-01-07 16:42:23 +0100

    fix: import

--- lib/screens/actions/change_profile.dart
+++ lib/screens/actions/change_profile.dart
@@ -1,6 +1,5 @@
 import 'package:denik_zza/screens/actions/profile.dart';
 import 'package:denik_zza/screens/login/components/my_button.dart';
-import 'package:denik_zza/screens/login/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class ChangeProfile extends StatelessWidget {
