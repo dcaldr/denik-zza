ae04c2a6745fd76c031dcb2805242319e21bd3df
Dan Caldr <cald01@vse.cz>
2024-01-07 18:26:42 +0100

    feat: quick edits of pdf(s)

--- lib/print_ops/dev_pdf_view.dart
+++ lib/print_ops/dev_pdf_view.dart
@@ -1,68 +1,23 @@
+import 'package:denik_zza/print_ops/pdf.dart';
 import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
-import 'dart:typed_data';
 
-class DevPdfView extends StatefulWidget {
-  final pw.Document pdf;
+import '../main.dart';
 
-  const DevPdfView({required this.pdf, super.key});
-
-  @override
-  _DevPdfViewState createState() => _DevPdfViewState();
-}
-
-class _DevPdfViewState extends State<DevPdfView> {
+class DevPdfView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("PDF Preview"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: _displayPdf,
-              child: const Text('Display PDF'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _displayPdf() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => PreviewScreen(pdf: widget.pdf)),
-    );
-  }
-}
-
-class PreviewScreen extends StatelessWidget {
-  final pw.Document pdf;
-
-  const PreviewScreen({required this.pdf, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back_outlined),
-        ),
-        centerTitle: true,
-        title: const Text("Preview"),
-      ),
       body: PdfPreview(
-        build: (format) async => pdf.save(),
+        build: (format) async => await PDFGenerator().generatePDFasSomething( notes)/* .save() */, // again hardcded 
         allowSharing: true,
         allowPrinting: true,
         initialPageFormat: PdfPageFormat.a4,
+        maxPageWidth: MediaQuery.of(context).size.height / 1.6, // hard coded -by hand
         pdfFileName: "sample.pdf",
       ),
     );
--- lib/print_ops/pdf.dart
+++ lib/print_ops/pdf.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
 
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
-import 'package:open_file/open_file.dart';
 
 class PDFGenerator {
   final bool _hideHeader = false;
-  final bool _hideBorders = true;
+  final bool _hideBorders = false;
   bool hideBody = false;
   //Colors // FIXME: to be refractored to a better place
   PdfColor myTransparentColor = PdfColor.fromHex("#FFFFFF00"); //transaprent
@@ -88,7 +86,7 @@ class PDFGenerator {
     );
   }
 
-  Future<void> generatePDF(String fileName, List<String> notes, [List<int> toSkip = const [2,]]) async {
+  Future<pw.Document> generatePDF(String fileName, List<String> notes, [List<int> toSkip = const [2]]) async {
     final mSafeFont = await PdfGoogleFonts.nunitoExtraLight();
     final pdf = pw.Document(
       theme: pw.ThemeData.withFont(
@@ -113,10 +111,38 @@ class PDFGenerator {
       ),
     );
 
-    final output = await pdf.save();
+/*     final output = await pdf.save();
     final file = File(fileName);
-    await file.writeAsBytes(output);
+    await file.writeAsBytes(output); 
 
-    OpenFile.open(fileName);
+    OpenFile.open(fileName);*/
+    return pdf;
+  }
+
+   generatePDFasSomething( List<String> notes,[List<int> toSkip = const [2]]) async {
+    final mSafeFont = await PdfGoogleFonts.nunitoExtraLight();
+    final pdf = pw.Document(
+      theme: pw.ThemeData.withFont(
+        base: mSafeFont,
+      ),
+    );
+
+    pw.Widget headerWidget = _generateHeader();
+
+    List<pw.Widget> noteTextWidgets = _generateNotes(notes,toSkip);
+    pw.Widget notesWidget = _generateNotesWidget(noteTextWidgets);
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Column(
+          children: [
+            headerWidget,
+            pw.SizedBox(height: 20),
+            notesWidget,
+          ],
+        ),
+      ),
+    );
+  return pdf.save();
   }
 }
