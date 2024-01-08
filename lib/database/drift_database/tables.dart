76dd70ebab3111fc8858458a39dcc196357fb497
Vojtěch Keder <kedv00@vse.cz>
2024-01-08 12:20:01 +0100

    add: Added file tables.dart with table definitions for drift database.

---
+++ lib/database/drift_database/tables.dart
@@ -0,0 +1,51 @@
+import 'package:drift/drift.dart';
+
+class InsuranceCompanies extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 0, max: 64).unique()();
+}
+
+class ZzaActions extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get actionTitle => text().withLength(min: 0, max: 128)();
+  TextColumn get actionDescription => text().withLength(min: 0, max: 1024).nullable()();
+  DateTimeColumn get dateFrom => dateTime()();
+  DateTimeColumn get dateTo => dateTime()();
+}
+
+class Participants extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get firstName => text().withLength(min: 0, max: 64)();
+  TextColumn get lastName => text().withLength(min: 0, max: 64)();
+  TextColumn get address => text().withLength(min: 0, max: 128)();
+  TextColumn get birthNumber => text().withLength(min: 0, max: 11)();
+  DateTimeColumn get birthDate => dateTime()();
+  TextColumn get parentPhoneNumber => text().withLength(min: 0, max: 13)();
+
+  // Foreign keys
+  IntColumn get insuranceCompany => integer().references(InsuranceCompanies, #id)();
+  IntColumn get zzaAction => integer().references(ZzaActions, #id)();
+}
+
+class Paramedics extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get firstName => text().withLength(min: 0, max: 64)();
+  TextColumn get lastName => text().withLength(min: 0, max: 64)();
+  TextColumn get address => text().withLength(min: 0, max: 128)();
+  DateTimeColumn get birthDate => dateTime()();
+  TextColumn get phoneNumber => text().withLength(min: 0, max: 13)();
+  TextColumn get username => text().withLength(min: 0, max: 64).unique()();
+}
+
+class Records extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  DateTimeColumn get dateAndTime => dateTime()();
+  TextColumn get title => text().withLength(min: 0, max: 64)();
+  TextColumn get description => text().withLength(min: 0, max: 512)();
+  TextColumn get treatment => text().withLength(min: 0, max: 512)();
+  BoolColumn get wasPrinted => boolean().withDefault(const Constant(false))();
+
+  // Foreign keys
+  IntColumn get paramedic => integer().references(Paramedics, #id)();
+  IntColumn get participant => integer().references(Participants, #id)();
+}
