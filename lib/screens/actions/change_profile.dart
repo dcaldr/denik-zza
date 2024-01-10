9e33aae003d499cb7bf8e49278be1e2a13c26ca3
Ester Stankovska <stankovskaester@gmail.com>
2024-01-10 22:37:02 +0100

    init: menu bar.

--- lib/screens/actions/change_profile.dart
+++ lib/screens/actions/change_profile.dart
@@ -1,11 +1,13 @@
+
 import 'package:denik_zza/screens/actions/profile.dart';
 import 'package:denik_zza/screens/login/components/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:denik_zza/screens/login/components/my_textfield.dart';
 
 class ChangeProfile extends StatelessWidget {
   ChangeProfile({Key? key});
-  
- final TextEditingController usernameController = TextEditingController();
+
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController lastnameController = TextEditingController();
   final TextEditingController loginNameController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
@@ -21,70 +23,97 @@ class ChangeProfile extends StatelessWidget {
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-
-              // name 
-              TextFormField(
-                controller: usernameController,
-                decoration: InputDecoration(labelText: 'Jmeno'),
-              ),
-              SizedBox(height: 10),
-
-              //
-              TextFormField(
-                controller: lastnameController,
-                decoration: InputDecoration(labelText: 'Prijmeni'),
-              ),
-              SizedBox(height: 10),
-
-              //
-              TextFormField(
-                controller: loginNameController,
-                decoration: InputDecoration(labelText: 'Prihlasovaci jmeno'),
-              ),
-              SizedBox(height: 10),
-              //
-
-              TextFormField(
-                controller: phoneNumberController,
-                decoration: InputDecoration(labelText: 'Telefonni cislo'),
-              ),
-              SizedBox(height: 20),
-
-              //
-              TextFormField(
-                controller: newPasswordController,
-                decoration: InputDecoration(labelText: 'Nove heslo'),
-              ),
-              SizedBox(height: 10),
-
-              //
-              TextFormField(
-                controller: newPasswordAgainController,
-                decoration: InputDecoration(labelText: 'Heslo znovu'),
-              ),
-              SizedBox(height: 10),
 
 
-              // button logic
-              Button(
-                onPressed: () {
-                   Navigator.push(
+      
+      drawer: Drawer(
+        // Your drawer content here
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('Profil', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+              onTap: () {
+                Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => Profile()), // profile for now, pop up
+                  MaterialPageRoute(builder: (context) => Profile()),
                 );
-                },
-                buttonText: "Potvrdit",
-                verticalPadding: 20,
-                horizontalPadding: 140,
+              },
+            ),
+            ListTile(
+              title: Text('Menu Item 2'),
+              onTap: () {
+                // Handle menu item 2
+              },
+            ),
+            // Add more menu items as needed
+          ],
+          
+        ),
+      ),
+
+
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Jméno',
+                    obscureText: false,
+                  ),
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: lastnameController,
+                    hintText: 'Příjmení',
+                    obscureText: false,
+                  ),
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: loginNameController,
+                    hintText: 'Přihlašovací jméno',
+                    obscureText: false,
+                  ),
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: phoneNumberController,
+                    hintText: 'Telefonní číslo',
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: newPasswordController,
+                    hintText: 'Nové heslo',
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 10),
+                  MyTextField(
+                    controller: newPasswordAgainController,
+                    hintText: 'Heslo znovu',
+                    obscureText: false,
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 10),
+
+            // button - temporarily navigates to the profile screen
+            Button(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Profile()),
+                );
+              },
+              buttonText: "Potvrdit",
+              verticalPadding: 20,
+              horizontalPadding: 142,
+            ),
+          ],
         ),
       ),
     );
