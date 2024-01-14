1180ca702a1f0d10dc683a17ded955ada498abd4
Teriaky22 <terezadzubova@gmail.com>
2024-01-14 18:05:21 +0100

    fix: changed path to screens for potvrdit and přihlásit se

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -5,6 +5,7 @@ import 'package:denik_zza/screens/login/components/create_account.dart';
 import 'package:denik_zza/screens/login/components/my_button.dart';
 import 'package:denik_zza/screens/login/components/my_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:denik_zza/screens/actions/all_actions.dart';
 
 class Login extends StatelessWidget {
   Login({super.key});
@@ -71,12 +72,16 @@ class Login extends StatelessWidget {
               Button(
                 onPressed: () {
                   if (usernameController.text == 'ester' &&
-                      passwordController.text == 'ester') {
-                    signUserIn(context);
-                  } else {
-                    print('Invalid username or password');
-                  }
-                },
+                    passwordController.text == 'ester') {
+                  // If the username and password are valid, navigate to the AllActions page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AllActions()),
+                  );
+                } else {
+                  print('Invalid username or password');
+                }
+              },
                 buttonText: "Prihlasit se",
                 verticalPadding: 20,
                 horizontalPadding: 130,
