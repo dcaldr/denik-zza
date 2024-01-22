bfa289c6cd5eff918e345f3cd62b059e923b16dd
Dan Caldr <cald01@vse.cz>
2024-01-22 21:08:59 +0100

    fix: ui problems #1

--- lib/print_ops/printer_ui.dart
+++ lib/print_ops/printer_ui.dart
@@ -1,9 +1,15 @@
 import 'package:denik_zza/print_ops/printer_woodoo.dart';
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+import 'dev_pdf_view.dart';
 
 class PageUI extends StatelessWidget {
-   PageUI({super.key});
-  PrinterWoodoo printer = PrinterWoodoo();
+  PageUI({super.key});
+  final PrinterWoodoo printer = PrinterWoodoo();
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +20,35 @@ class PageUI extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                  onPressed: () {
-            printer.printAll();
-                   },
-                child: Text('tisk všecho'),
-),
+              FutureBuilder<PrintPack>(
+                future: printer.printAll(),
+                builder: (BuildContext context, AsyncSnapshot<PrintPack> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator(); // Show a loading spinner while waiting
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}'); // Show error if something went wrong
+                  } else {
+                    var myPDF = snapshot.data!.finishedDocument.save();
+                    return ElevatedButton(
+                      onPressed: () {
+                        myPDF.then((value) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) =>
+                                PdfPreview(build: (format) async => value,
+                                  allowSharing: true,
+                                  allowPrinting: true,
+                                  initialPageFormat: PdfPageFormat.a4,
+                                  maxPageWidth: MediaQuery.of(context).size.height / 1.6, // hard coded -by hand
+                                  pdfFileName: "sample.pdf",)),
+                          );
+                        });
+                      },
+                      child: Text('tisk všecho'),
+                    );
+                  }
+                },
+              ),
               SizedBox(height: 10), // Add space between buttons
               ElevatedButton(
                 onPressed: () {},
