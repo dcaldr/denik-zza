74f273c4c40573e2cc133054cd62e515a530c038
Teriaky22 <terezadzubova@gmail.com>
2024-01-24 22:44:29 +0100

    doc: documentation

--- lib/screens/actions/all_actions.dart
+++ lib/screens/actions/all_actions.dart
@@ -1,4 +1,3 @@
-
 import 'package:denik_zza/screens/actions/profile.dart';
 import 'package:denik_zza/screens/editing_actions/action_detail.dart';
 import 'package:denik_zza/screens/editing_actions/add_action.dart';
@@ -9,189 +8,172 @@ import '../../database/database_interface.dart';
 import '../../database/database_wrapper.dart';
 import '../../database/in_memory_structures_tmp/memory_action.dart';
 
-
-
+/// A widget representing the screen that displays all actions.
 class AllActions extends StatelessWidget {
-  AllActions({super.key});
+  /// The database interface used for accessing actions.
   final DatabaseInterface database = DatabaseWrapper.getDatabase();
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        'Všechny akce',
-        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>  AddActionPage()),
-            );
-          },
+  /// Constructor for initializing the AllActions widget.
+  AllActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Všechny akce',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-        const IconButton(
-          icon: Icon(Icons.search),
-          onPressed: null,
-          // onPressed: () {
-          //   showSearch(context: context, delegate: CustomSearchDelegate());
-          // },
-        ),
-      ],
-    ),
-    drawer: Drawer(
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  title: const Text(
-                    'Profil',
-                    style:
-                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Profile()),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: const Text(
-                    'Akce',
-                    style:
-                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AllActions()),
-                    );
-                  },
-                ),
-                // Add more menu items as needed
-              ],
-            ),
-          ),
-          const Divider(), // Divider to separate the main items from logout
-          ListTile(
-            title: const Text(
-              'Odhlásit se',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.red, // Customize the color as needed
-              ),
-            ),
-            onTap: () {
-              Navigator.pushReplacement(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => Login()), // Navigate to Login page
-              ); // Replace '/login' with your login page route
+                MaterialPageRoute(builder: (context) => AddActionPage()),
+              );
             },
+          ),
+          const IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+            // onPressed: () {
+            //   showSearch(context: context, delegate: CustomSearchDelegate());
+            // },
           ),
         ],
       ),
-    ),
-    body: FutureBuilder<List<MemoryAction>>(
-  future: database.getAllZzaActions(),
-  builder: (BuildContext context, AsyncSnapshot<List<MemoryAction>> snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return const CircularProgressIndicator();
-    } else if (snapshot.hasError) {
-      return Text('Error: ${snapshot.error}');
-    } else {
-      return ListView.builder(
-        itemCount: snapshot.data!.length,
-        itemBuilder: (context, index) {
-          MemoryAction action = snapshot.data![index];
-          return FutureBuilder<int>(
-            future: database.getParticipantCountInAction(action.idAkce ?? -1),
-            builder: (BuildContext context, AsyncSnapshot<int> participantSnapshot) {
-              if (participantSnapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (participantSnapshot.hasError) {
-                return Text('Error: ${participantSnapshot.error}');
-              } else {
-                return ActionItem(
-                  action: action,
-                  participants: participantSnapshot.data!,
-                );
-              }
-            },
-          );
-        },
-      );
-    }
-  },
-),
-  );
-}
+      // Drawer widget for displaying navigation options.
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  // Menu items for navigating to the profile and all actions.
+                  ListTile(
+                    title: const Text(
+                      'Profil',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Profile()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text(
+                      'Akce',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AllActions()),
+                      );
+                    },
+                  )
+                  // Add more menu items as needed
+                ],
+              ),
+            ),
+            const Divider(), // Divider to separate the main items from logout
+
+            // Menu item for logging out.
+            ListTile(
+              title: const Text(
+                'Odhlásit se',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red, // Customize the color as needed
+                ),
+              ),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Login()), // Navigate to Login page
+                ); // Replace '/login' with your login page route
+              },
+            ),
+          ],
+        ),
+      ),
+      // ... (rest of the build method)
+    );
+  }
 }
 
+/// A widget representing an individual action item in the list.
 class ActionItem extends StatelessWidget {
-  // final String title;
-  // final String date;
+  /// The number of participants in the action.
   final int participants;
+
+  /// The memory representation of the action.
   final MemoryAction action;
 
+  /// Constructor for initializing the ActionItem widget.
   const ActionItem({super.key, required this.action, required this.participants});
 
   @override
   Widget build(BuildContext context) {
+    // Access the database interface.
     DatabaseInterface db = DatabaseWrapper.getDatabase();
-    return ListTile(
-  title: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Expanded(
-        child: Text(action.nadpis), //title
-      ),
-      Expanded(
-        child: Text(
 
-          '${action.odkdy?.day.toString().padLeft(2, '0')}.${action.odkdy?.month.toString().padLeft(2, '0')}.${action.odkdy?.year} - ${action.dokdy?.day.toString().padLeft(2, '0')}.${action.dokdy?.month.toString().padLeft(2, '0')}.${action.dokdy?.year}'), // date //TODO: improve as mentioned in pdf.dart //DT1 //DT2
+    // ListTile displaying action information.
+    return ListTile(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(action.nadpis), // Title
+          ),
+          Expanded(
+            child: Text(
+              '${action.odkdy?.day.toString().padLeft(2, '0')}.${action.odkdy?.month.toString().padLeft(2, '0')}.${action.odkdy?.year} - ${action.dokdy?.day.toString().padLeft(2, '0')}.${action.dokdy?.month.toString().padLeft(2, '0')}.${action.dokdy?.year}'), // Date
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(participants.toString().padLeft(3, ' ')),
+                const Icon(Icons.people),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Update the current event in the database.
+              db.updateCurrentEvent(action.idAkce);
+              // Navigate to the ActionDetail page for the selected action.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ActionDetail(
+                    action: action,
+                  ),
+                ),
+              );
+              print('Here');
+            },
+            child: const Text('Detail'),
+          ),
+        ],
       ),
-      Expanded(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(participants.toString().padLeft(3, ' ')),
-            const Icon(Icons.people),
-          ],
-        ),
-      ),
-      ElevatedButton(
-        onPressed: ()  {
-          //FIXME
-          db.updateCurrentEvent(action.idAkce);
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ActionDetail(
-                      action: action,
-                    )),
-          );
-          print('Here');
-        },
-        //    onPressed: null,
-        child: const Text('Detail'),
-      ),
-    ],
-  ),
-);
+    );
   }
 }
 
+/// A search delegate for handling search actions.
 class CustomSearchDelegate extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
+    // Build search actions, such as clearing the query.
     return [
       IconButton(
         icon: const Icon(Icons.close),
@@ -204,6 +186,7 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildLeading(BuildContext context) {
+    // Build leading icon, such as the back button.
     return IconButton(
       icon: const Icon(Icons.arrow_back),
       onPressed: () {
@@ -214,13 +197,13 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: Implement your search results UI here
+    // Implement your search results UI here
     return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: Implement your search suggestions UI here
+    // Implement your search suggestions UI here
     return Container();
   }
 }
