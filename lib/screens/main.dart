4cf01e1693a75b84ed5745b18e0b845669b02c9d
Ester Stankovska <stankovskaester@gmail.com>
2024-01-24 23:09:25 +0100

    doc: main.

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -2,10 +2,12 @@ import 'package:denik_zza/screens/login/login_page.dart';
 import 'package:denik_zza/screens/login/register_page.dart';
 import 'package:flutter/material.dart';
 
+// Main entry point of the application
 void main() {
   runApp(MyApp());
 }
 
+// The root widget of the application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
