3b241c59f9952e6f4662a8ce02962f928f9ef556
Dan Caldr <cald01@vse.cz>
2024-01-25 02:43:04 +0100

    add: printUI to our_drawer.dart

--- lib/screens/our_widgets/our_drawer.dart
+++ lib/screens/our_widgets/our_drawer.dart
@@ -1,10 +1,13 @@
 
 
+import 'package:denik_zza/print_ops/printer_ui.dart';
 import 'package:flutter/material.dart';
 
 import '../actions/all_actions.dart';
 import '../actions/profile.dart';
-
+///One place for Drawer if necerssary
+///
+/// Our version of drawer //TODO: add icons or something
 class OurDrawer extends StatelessWidget {
   const OurDrawer({
     super.key,
@@ -15,10 +18,10 @@ class OurDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          ListTile(
+          ListTile( //todo:  extract widget -> prevent repeating code
             title: const Text(
               'Profil',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+             // style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
             onTap: () {
               Navigator.push(
@@ -30,7 +33,7 @@ class OurDrawer extends StatelessWidget {
           ListTile(
             title: const Text(
               'Akce',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+           //   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
             onTap: () {
               Navigator.push(
@@ -39,6 +42,15 @@ class OurDrawer extends StatelessWidget {
               );
             },
           ),
+          ListTile(
+            title: const Text('Tisk'),
+            onTap:(){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PageUI())
+              );
+            },
+          )
         ],
       ),
     );
