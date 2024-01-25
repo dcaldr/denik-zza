301d515eabd692bf85482176788c5e2c2cda97f1
Teriaky22 <terezadzubova@gmail.com>
2024-01-25 07:48:40 +0100

    doc: pass

--- lib/screens/login/components/forgot_password.dart
+++ lib/screens/login/components/forgot_password.dart
@@ -1,30 +1,30 @@
-import 'package:denik_zza/screens/login/forgot_password_page.dart';
-import 'package:flutter/material.dart';
+//import 'package:denik_zza/screens/login/forgot_password_page.dart';
+//import 'package:flutter/material.dart';
 
 /// Widget representing a "Forgot Password" link.
-class ForgotPassword extends StatelessWidget {
+//class ForgotPassword extends StatelessWidget {
   /// Constructor for the ForgotPassword widget.
-  const ForgotPassword({Key? key});
+  //const ForgotPassword({Key? key});
 
   /// Builds the UI for the ForgotPassword widget.
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
+ // @override
+  //Widget build(BuildContext context) {
+    //return GestureDetector(
+      //onTap: () {
         // Redirect to another screen (ForgotPasswordScreen)
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ForgotPasswordScreen()), 
-        );
-      },
-      child: Text(
-        'Zapomenuté heslo',
-        style: TextStyle(
-          color: Color.fromARGB(103, 0, 0, 0),
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
-}
+        //Navigator.push(
+          //context,
+          //MaterialPageRoute(builder: (context) => ForgotPasswordScreen()), 
+        //);
+     // },
+      //child: Text(
+        //'Zapomenuté heslo',
+        //style: TextStyle(
+          //color: Color.fromARGB(103, 0, 0, 0),
+          //fontSize: 16,
+          //fontWeight: FontWeight.bold,
+        //),
+      //),
+    //);
+  //}
+//}
