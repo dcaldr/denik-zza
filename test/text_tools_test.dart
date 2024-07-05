1d21198f3936ff14b61f7c8a73b0bb5dc7c6ec18
Dan Caldr <cald01@vse.cz>
2024-07-05 16:21:00 +0200

    feat: text_tools.dart init

---
+++ test/text_tools_test.dart
@@ -0,0 +1,27 @@
+
+import 'package:denik_zza/input/text_tools.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final TextTools textTools = TextTools();
+  group('basic tests too see how build-in options work', () {
+    //test(description, body)
+    test('basic string trimming',() {
+      expect('     hello    '.trim(), 'hello');
+    });
+    test('to lowercase',(){
+      expect("HeLlO".toLowerCase(), 'hello');
+    });
+    test('to lowercase harder',(){
+      expect('ĚŠČŘŽÝÁÍÉŮĎˇI'.toLowerCase(), 'ěščřžýáíéůďˇi');
+    });
+
+
+
+  });
+  group('advanced text manipulation',(){
+    test('diacritics remove',(){
+      expect('ěščřžýáíéúůďť','escrzyaieuudt');
+    });
+  });
+  }
