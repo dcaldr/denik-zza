bd53bd74b090242c1b722bab85d6f75325f624c2
Dan Caldr <cald01@vse.cz>
2024-07-15 10:33:21 +0200

    feat: converters finished

--- lib/input/text_tools.dart
+++ lib/input/text_tools.dart
@@ -28,6 +28,54 @@ class TextTools{
      }
      return false;
    }
+   static DateTime? parseDate(String date) {
+     DateTime? parsedDate;
+     try {
+       parsedDate = DateTime.parse(date);
+     } catch (e) {
+       parsedDate = null;
+     }
+     if (parsedDate != null) {
+       return myParseDate(date);
+     }
+
+
+
+     return null;
+   }
+   static myParseDate(String date){
+     DateTime? parsedDate;
+     // split text
+     List<String> splitted= date.split(RegExp(r'[-/.\s]'));
+      if(splitted.length != 3){
+        return null;
+      }
+      // try to parse
+
+     if(splitted[0].length == 4){
+       //1) yyyy.mm.dd
+       try {
+         parsedDate = DateTime(int.parse(splitted[0]), int.parse(splitted[1]), int.parse(splitted[2]));
+       } catch (e) {
+         parsedDate = null;
+       }
+       if (parsedDate != null) {
+         return parsedDate;
+       }
+     }
+
+     //1) dd.mm.yyyy
+      try {
+        parsedDate = DateTime(int.parse(splitted[2]), int.parse(splitted[1]), int.parse(splitted[0]));
+      } catch (e) {
+        parsedDate = null;
+      }
+      if (parsedDate != null) {
+        return parsedDate;
+      }
+
+
+   }
 
 
 
