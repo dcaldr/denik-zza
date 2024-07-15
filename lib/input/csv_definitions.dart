f85c8fb0cb0ee61054d5da310b14da836225f99d
Dan Caldr <cald01@vse.cz>
2024-07-15 15:12:44 +0200

    fix: constructor logic in input_hold.dart

--- lib/input/csv_definitions.dart
+++ lib/input/csv_definitions.dart
@@ -1,22 +1,19 @@
 import 'package:denik_zza/input/input_hold.dart';
 
+
+/// defines variants for csv files
 class CsvDefinitions{
   List<InputHold> mainCsv = [
-    JmenoHold("CHANGEME", "jméno"),
-    JmenoHold("CHANGEME", "příjmení"),
-    PohlaviHold("CHANGEME", columnName: "pohlaví"),
-    AdresaHold("CHANGEME", columnName: "adresa"),
-    CisloPojisteniHold("CHANGEME", columnName: "rodné číslo"),
-    DatumNarozeniHold("CHANGEME", columnName: "datum narození"),
-    TelefonHold("changeme"),
-     EmailHold("changeme"),
-    PotvrzeniHold("CHANGEME",  "způsobilost"),
-    PojistovnaHold("CHANGEME", "pojišťovna"),
-    TextHold("CHANGEME", "poznámka"),
-
-
-
-
-
-  ];
+  JmenoHold(columnName: "jméno"),
+  JmenoHold(columnName: "příjmení"),
+  PohlaviHold(columnName: "pohlaví"),
+  AdresaHold(columnName: "adresa"),
+  CisloPojisteniHold(columnName: "rodné číslo"),
+  DatumNarozeniHold(columnName: "datum narození"),
+  TelefonHold(columnName: "telefon rodič"),
+  EmailHold(columnName: "email rodič"),
+  PotvrzeniHold(columnName: "způsobilost"),
+  PojistovnaHold(columnName: "pojišťovna"),
+  TextHold(columnName: "poznámka"),
+];
 }
