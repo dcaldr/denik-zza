9397a04805c950da39323893d39aa21b07d25c69
Dan Caldr <cald01@vse.cz>
2024-07-21 00:49:45 +0200

    fix: handling isTesting in file_manager.dart

--- lib/input/file_manager.dart
+++ lib/input/file_manager.dart
@@ -14,45 +14,26 @@ class FileManager {
   static final FileManager _instance = FileManager._internal();
   static const String homeFolderName = 'Deník ZZA';
   Directory? homeDir;
-  /// defines if in testing environment, where creating a lot of dirs is not desirable
-  bool isTesting = false;
+  bool isTesting;
   List<String> subFolders = ['backup', 'zpusobilosti', 'vysetreni'];
 
-  /// Constructor
-  ///
-  /// Takes care of detecting if it's in testing context
-  FileManager._internal({this.homeDir, bool? bypassTesting}) {
-    if (bypassTesting == false || bypassTesting == null) {
-      assert(() {
-        isTesting = true;
-        subFolders = [];
-        return true;
-      }());
-    }
-  }
+  FileManager._internal() : isTesting = false;
 
-  /// Factory constructor
-  ///
-  /// Initializes the singleton instance and sets the home directory and testing mode
-  factory FileManager({Directory? homeDir, bool? bypassTesting}) {
+  factory FileManager({Directory? homeDir, bool? isTesting}) {
     _instance.homeDir = homeDir;
-    if (bypassTesting != null) {
-      _instance.isTesting = bypassTesting;
+    _instance.isTesting = isTesting ?? false;
+    if (_instance.isTesting) {
+      _instance.subFolders = [];
+    } else {
+      _instance.subFolders = ['backup', 'zpusobilosti', 'vysetreni']; //FIXME: dirt fix - duplicate code
     }
     return _instance;
   }
 
-  /// Gets "home" data directory
-  ///
-  /// Returns the home directory if it exists, otherwise creates it
   Future<Directory?> getHomeDir() async {
-    return homeDir ?? await createHomeDataDir();
+    return isTesting ? null : (homeDir ?? await createHomeDataDir());
   }
 
-  /// Creates or leaves home directory under ApplicationDocumentsDirectory
-  ///
-  /// Can be overridden by [inputPath] that must exist prior.
-  /// In testing, defaults to current Directory
   Future<Directory?> createHomeDataDir([String? inputPath]) async {
     if (isTesting) return null;
     if (homeDir != null && (inputPath == null || inputPath == homeDir?.path)) {
@@ -74,10 +55,6 @@ class FileManager {
     return thisDir;
   }
 
-  /// For given event, create new folder directory structure
-  ///
-  /// When given existing name, it chooses a new name to prevent collision.
-  /// Returns accepted directory name or null if failed
   Future<Directory?> createNewEventDataDir(String eventFolderName) async {
     if (isTesting) return null;
     Directory? currentDir = await getHomeDir();
@@ -96,9 +73,6 @@ class FileManager {
     return createSubfolders(eventDirCandidate);
   }
 
-  /// Creates subfolders in given (event) directory
-  ///
-  /// Returns inputted directory or null if failed
   Future<Directory?> createSubfolders(Directory baseDir) async {
     if (isTesting) return null;
     for (var subFolder in subFolders) {
@@ -113,10 +87,6 @@ class FileManager {
     return baseDir;
   }
 
-  /// Checks if file/folder exists and suggests new name if needed
-  ///
-  /// Given [base] path checks if [inName] would collide with existing file/folder
-  /// and if so suggests new name
   Future<String?> nameCollisionSolver(Directory base, String inName) async {
     if (isTesting) return null;
     final logger = Logger();
@@ -144,77 +114,14 @@ class FileManager {
     }
   }
 
-  /// Gets db file path
-  ///
-  /// If it's used in testing context, it returns null
   Future<String?> getDbFilePath() async {
     if (isTesting) return null;
     final homeDir = await getHomeDir();
     return homeDir?.path;
   }
 
-  /// Sync version of getDbFilePath
-  ///
-  /// WARN\! Doesn't create folder structure
   @Deprecated('Use getDbFilePath() instead')
   String? getDbFilePathSync() {
-    return homeDir?.path;
-  }
-}
-
-/// Class to manage event files
-///
-/// Provides methods to upload files, test folder structure, and change events
-class EventFiles {
-  String? currentEventName;
-  Directory? currentEventDir;
-
-  /// Uploads a file to the current event directory
-  ///
-  /// Throws [UnimplementedError] as the method is not yet implemented
-  Future<String?> uploadZpusobilost(File file) async {
-    throw UnimplementedError();
-  }
-
-  /// Tests the structure of the current event directory
-  ///
-  /// Returns true if all subfolders exist, false otherwise
-  bool testStructure() {
-    if (currentEventDir != null) {
-      bool isOk = true;
-      for (var subFolder in FileManager().subFolders) {
-        Directory subDir = Directory('${currentEventDir?.path}/$subFolder');
-        if (!subDir.existsSync()) {
-          Logger().e('Error: $subFolder does not exist');
-          isOk = false;
-        }
-      }
-      return isOk;
-    }
-    return false;
-  }
-
-  /// Call when user changes event
-  ///
-  /// Updates current event name and directory
-  Future<void> changeEvent() async {
-    DatabaseInterface mydb = DatabaseWrapper.getDatabase();
-    MemoryAction? currAction = await mydb.getCurrentAction();
-    if (currAction != null) {
-      currentEventName = currAction.nadpis;
-      String? path = currAction.domovskyAdresarPath;
-      currentEventDir = path != null ? Directory(path) : null;
-    }
-  }
-
-  /// Call when user creates new event
-  ///
-  /// Updates current event name and directory
-  Future<MemoryAction?> initNewEvent(MemoryAction? event) async {
-    FileManager fm = FileManager();
-    if (event != null) {
-      event.domovskyAdresarPath = await fm.createNewEventDataDir(event.nadpis).then((value) => value?.path);
-    }
-    return event;
+    return isTesting ? null : homeDir?.path;
   }
 }
