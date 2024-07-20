66330b8073e5d3190fc341048e544ee8c899fcbe
Dan Caldr <cald01@vse.cz>
2024-07-20 22:08:31 +0200

    add: logic for allowing appending pdf

--- lib/print_ops2/generate_pdf_template.dart
+++ lib/print_ops2/generate_pdf_template.dart
@@ -2,6 +2,7 @@ import 'package:denik_zza/print_ops2/print_pdf_header.dart';
 import 'package:denik_zza/print_ops2/print_pdf_records.dart';
 import 'package:denik_zza/print_ops2/print_pdf_restrictions.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:logger/logger.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
@@ -23,6 +24,14 @@ class GeneratePdfTemplate {
   List<Omezeni>? omezeniList;
   List<MemoryLek>? lekList;
   List<MemoryZaznam>? zaznamList;
+  /// status for the restrictions
+  OkCodes _omezeniStatus =OkCodes.unset;
+  /// status for the medications
+  OkCodes _lekStatus = OkCodes.unset;
+  /// status for both restrictions and medications
+  OkCodes _allRestrictionsStatus = OkCodes.unset;
+
+  OkCodes _recordStatus = OkCodes.unset;
 
   GeneratePdfTemplate();
 
@@ -32,6 +41,123 @@ class GeneratePdfTemplate {
     this.lekList,
     this.zaznamList,
   });
+
+  /// tests if appending is possible or needs to be completely recreated
+  ///
+  /// returns true if the given wasPrinted flags are in good configuration
+  /// Header T -> Restrictions(all) T -> Records T(all but lasts) - ok
+  /// Header T -> Restrictions(all) F -> Records F - ok
+  /// Header T -> Restrictions(some) F -> Records T - not ok
+  bool canAppend(){
+   if (osoba == null || !(osoba?.wasPrinted ?? false)) {
+  return false;
+}
+   // here osoba(header) always printed
+    isRestrictionsOk();
+    isRecordsOk();
+    // if any is broken return false
+    if(_allRestrictionsStatus == OkCodes.broken || _recordStatus == OkCodes.broken){
+      return false;
+    }
+
+    // restriction block - false && records block - false --> ok
+    if(_allRestrictionsStatus == OkCodes.unprinted && _recordStatus == OkCodes.unprinted){
+      return true;
+    }
+    // restriction block - true && records block - true --> ok
+    if(_allRestrictionsStatus == OkCodes.printed && _recordStatus == OkCodes.printed){
+      return true;
+    }
+
+    return false;
+  }
+
+
+/// tests if restrictions and medications aren't blocking appending
+  void isRestrictionsOk(){
+    // preventive
+    // if(_allRestrictionsStatus != OkCodes.unset){
+    //   return;
+    // }
+    if (_omezeniStatus == OkCodes.unset) {
+      bool start = omezeniList!.first.wasPrinted;
+      for (var item in omezeniList!) {
+        // if start changes value return broken
+        if(start != item.wasPrinted){
+          _omezeniStatus = OkCodes.broken;
+          return;
+        }
+      }
+       _omezeniStatus = start ? OkCodes.printed : OkCodes.unprinted;
+    }
+    if (_lekStatus == OkCodes.unset) {
+      bool start = lekList!.first.wasPrinted;
+      for (var item in lekList!) {
+        // if start changes value return broken
+        if(start != item.wasPrinted){
+          _lekStatus = OkCodes.broken;
+          return;
+        }
+      }
+       _lekStatus = start ? OkCodes.printed : OkCodes.unprinted;
+    }
+    /// either both are the same or are broken
+    _allRestrictionsStatus = _omezeniStatus == _lekStatus ? _omezeniStatus : OkCodes.broken;
+
+
+  }
+
+  /// tests if records aren't blocking appending
+  ///
+  /// for entire safety it will order the list by time (but it should be already ordered)
+  void isRecordsOk(){
+    if(_recordStatus != OkCodes.unset){
+      return;
+    }
+    if(zaznamList == null){
+      _recordStatus = OkCodes.unprinted;
+      return;
+    }
+
+    // if in testing mode check and Logger warn if not ordered
+ assert(() {
+  var cmpList = List<MemoryZaznam>.from(zaznamList!);
+  zaznamList!.sort((a, b) => a.casZaznamu!.compareTo(b.casZaznamu!));
+  for (int i = 0; i < cmpList.length; i++) {
+    if (cmpList[i] != zaznamList![i]) {
+      Logger().w("Records are not ordered by time, where expected in GeneratePdfTemplate");
+      return true;
+    }
+  }
+  return true;
+}());
+
+
+    /// order by time of the record (oldest first)
+    zaznamList!.sort((a, b) => a.casZaznamu!.compareTo(b.casZaznamu!));
+
+
+
+
+
+    /// after finding first true all the rest should be true or its broken
+    bool start = zaznamList!.first.isPrinted;
+    bool prevItem = zaznamList!.first.isPrinted;
+    for (var item in zaznamList!) {
+      // if isPrinted after some that wasn't printed
+      if(item.isPrinted && !prevItem){
+        _recordStatus = OkCodes.broken;
+        return;
+      }
+      prevItem = item.isPrinted;
+    }
+
+
+    /// for here : if last item is printed then all are printed
+    _recordStatus = prevItem ? OkCodes.printed : OkCodes.unprinted;
+  }
+
+
 /// generates list of pages for pdf from the given person
   Future<List<pw.Page>> getPdfPages({
     required MemoryOsoba osoba,
@@ -79,4 +205,10 @@ class GeneratePdfTemplate {
 
 abstract class PdfSection {
   pw.Widget buildSection(bool append);
+}
+enum OkCodes {
+  unprinted,
+  printed,
+  broken,
+  unset,
 }
