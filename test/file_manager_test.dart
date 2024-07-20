f5d5eaef2610c3241fa3bac4f5a012de32b27b82
Dan Caldr <cald01@vse.cz>
2024-07-21 00:50:07 +0200

    test: added file_manager_test.dart

---
+++ test/file_manager_test.dart
@@ -0,0 +1,47 @@
+
+
+import 'dart:ui';
+
+import 'package:denik_zza/input/file_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+var loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
+
+//TODO: path provider needs to be mocked or bypassed as https://github.com/flutter/packages/blob/main/packages/path_provider/path_provider/test/path_provider_test.dart
+main(){
+  TestWidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  group('FileManager bypass test  logic',(){
+    test('bypass test  true',() async {
+      FileManager fileManager = FileManager(isTesting: true);
+      expect(fileManager, isNotNull);
+      expect(fileManager.isTesting, isTrue);
+      expect(fileManager.subFolders, isEmpty);
+      await fileManager.createHomeDataDir();
+      expect(fileManager.homeDir, isNull);
+    });
+    test('bypass test  false',() async {
+      FileManager fileManager = FileManager(isTesting: false);
+      expect(fileManager, isNotNull);
+      expect(fileManager.isTesting, isFalse);
+      expect(fileManager.subFolders, isNotEmpty);
+      // await fileManager.createHomeDataDir();
+      // expect(fileManager.homeDir, isNotNull);
+    });
+    test('bypass test  false2',() async {
+      FileManager fileManager = FileManager();
+      expect(fileManager, isNotNull);
+      expect(fileManager.isTesting, isFalse);
+      expect(fileManager.subFolders, isNotEmpty);
+      // await fileManager.createHomeDataDir();
+      // expect(fileManager.homeDir, isNotNull);
+    });
+  });
+}
