3c6d08160174b5129b0dc7702ef1977dbc97eecd
Dan Caldr <cald01@vse.cz>
2024-07-27 14:20:02 +0200

    fix: participant_registration_form.dart passes values to validator now

--- lib/screens2/participant_registration_form.dart
+++ lib/screens2/participant_registration_form.dart
@@ -97,153 +97,151 @@ class _ParticipantRegistrationFormState
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Registrace Účastníka')),
-      drawer: const AppDrawer(),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              GridView.count(
-                crossAxisCount: 3,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                mainAxisSpacing: 4.0,
-                crossAxisSpacing: 4.0,
-                childAspectRatio: 3 / 1,
-                children: [
-                  TextFormField(
-                    controller: _controllers['jmeno'],
-                    decoration: const InputDecoration(labelText: 'Jméno'),
-                    validator: (value) {
-                      final validator = _validators['jmeno']?.validator();
-                      if (value == null || value.isEmpty) {
-                        return 'Jméno je povinné pole';
-                      }
-                      return validator;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _controllers['prijmeni'],
-                    decoration: const InputDecoration(labelText: 'Příjmení'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Příjmení je povinné pole';
-                      }
-                      return _validators['prijmeni']?.validator();
-                    },
-                  ),
-                  TextFormField(
-                    controller: _controllers['cisloPojisteni'],
-                    decoration:
-                        const InputDecoration(labelText: 'Číslo Pojištěnce'),
-                    validator: (value) =>
-                        _validators['cisloPojisteni']?.validator(),
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: CustomDatePicker(
-                          controller: _controllers['datumNarozeni']!,
-                          labelText: 'Datum Narození',
-                        ),
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Registrace Účastníka')),
+    drawer: const AppDrawer(),
+    body: Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            GridView.count(
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+              childAspectRatio: 3 / 1,
+              children: [
+                TextFormField(
+                  controller: _controllers['jmeno'],
+                  decoration: const InputDecoration(labelText: 'Jméno'),
+                  validator: (value) {
+                    final validator = _validators['jmeno']?.validator(value);
+                    if (value == null || value.isEmpty) {
+                      return 'Jméno je povinné pole';
+                    }
+                    return validator;
+                  },
+                ),
+                TextFormField(
+                  controller: _controllers['prijmeni'],
+                  decoration: const InputDecoration(labelText: 'Příjmení'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Příjmení je povinné pole';
+                    }
+                    return _validators['prijmeni']?.validator(value);
+                  },
+                ),
+                TextFormField(
+                  controller: _controllers['cisloPojisteni'],
+                  decoration:
+                      const InputDecoration(labelText: 'Číslo Pojištěnce'),
+                  validator: (value) =>
+                      _validators['cisloPojisteni']?.validator(value),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: CustomDatePicker(
+                        controller: _controllers['datumNarozeni']!,
+                        labelText: 'Datum Narození',
                       ),
-                      const SizedBox(width: 8.0),
-                      Expanded(
-                        child: TextFormField(
-                          controller: _controllers['pohlavi'],
-                          decoration: const InputDecoration(labelText: 'Pohlaví'),
-                          validator: (value) =>
-                              _validators['pohlavi']?.validator(),
-                        ),
+                    ),
+                    const SizedBox(width: 8.0),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _controllers['pohlavi'],
+                        decoration: const InputDecoration(labelText: 'Pohlaví'),
+                        validator: (value) =>
+                            _validators['pohlavi']?.validator(value),
                       ),
-                    ],
-                  ),
-                  TextFormField(
-                    controller: _controllers['zdravotniPojistovna'],
-                    decoration: const InputDecoration(
-                        labelText: 'Zdravotní Pojišťovna'),
-                    validator: (value) =>
-                        _validators['zdravotniPojistovna']?.validator(),
-                  ),
-                  TextFormField(
-                    controller: _controllers['adresa'],
-                    decoration: const InputDecoration(labelText: 'Adresa'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Adresa je povinné pole';
-                      }
-                      return _validators['adresa']?.validator();
-                    },
-                  ),
-                  TextFormField(
-                    controller: _controllers['jmenoRodice'],
-                    decoration:
-                        const InputDecoration(labelText: 'Jméno rodiče'),
-                    validator: (value) =>
-                        _validators['jmenoRodice']?.validator(),
-                  ),
-                  TextFormField(
-                    controller: _controllers['emailRodice'],
-                    decoration:
-                        const InputDecoration(labelText: 'Email rodiče'),
-                    validator: (value) =>
-                        _validators['emailRodice']?.validator(),
-                  ),
-                  TextFormField(
-                    controller: _controllers['telefonRodice'],
-                    decoration:
-                        const InputDecoration(labelText: 'Telefon rodiče'),
-                    validator: (value) =>
-                        _validators['telefonRodice']?.validator(),
-                  ),
-                ],
-              ),
-              TextFormField(
-                controller: _controllers['poznamka'],
-                decoration: const InputDecoration(
-                    labelText: 'Poznámka',
-                    border: OutlineInputBorder(),
-                    hintText: 'Tento text se nebude tisknout'),
-                maxLines: 3,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    MemoryOsoba osoba = MemoryOsoba.fullNamed(
-                      id: null,
-                      jmeno: _controllers['jmeno']!.text,
-                      prijmeni: _controllers['prijmeni']!.text,
-                      datumNarozeni: DateFormat('dd.MM.yyyy')
-                          .parse(_controllers['datumNarozeni']!.text),
-                      adresa: _controllers['adresa']!.text,
-                      cisloPojisteni: _controllers['cisloPojisteni']!.text,
-                      jmenoRodice: _controllers['jmenoRodice']!.text,
-                      telefonRodice: _controllers['telefonRodice']!.text,
-                      emailRodice: _controllers['emailRodice']!.text,
-                      zdravotniPojistovna:
-                          _controllers['zdravotniPojistovna']!.text,
-                      poznamka: _controllers['poznamka']!.text,
-                      pohlavi: int.parse(_controllers['pohlavi']!.text),
-                      zpusobilost: false,
-                      bezinfekcnost: false,
-                      wasPrinted: false,
-                      oddil: '',
-                      prisel: false,
-                      potvrzeniPath: '',
-                    );
-                    _showPersonDetails(osoba);
-                  }
-                },
-                child: const Text('Odeslat'),
-              ),
-            ],
-          ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  controller: _controllers['zdravotniPojistovna'],
+                  decoration: const InputDecoration(
+                      labelText: 'Zdravotní Pojišťovna'),
+                  validator: (value) =>
+                      _validators['zdravotniPojistovna']?.validator(value),
+                ),
+                TextFormField(
+                  controller: _controllers['adresa'],
+                  decoration: const InputDecoration(labelText: 'Adresa'),
+                  validator: (value) {
+
+                    return _validators['adresa']?.validator(value);
+                  },
+                ),
+                TextFormField(
+                  controller: _controllers['jmenoRodice'],
+                  decoration:
+                      const InputDecoration(labelText: 'Jméno rodiče'),
+                  validator: (value) =>
+                      _validators['jmenoRodice']?.validator(value),
+                ),
+                TextFormField(
+                  controller: _controllers['emailRodice'],
+                  decoration:
+                      const InputDecoration(labelText: 'Email rodiče'),
+                  validator: (value) =>
+                      _validators['emailRodice']?.validator(value),
+                ),
+                TextFormField(
+                  controller: _controllers['telefonRodice'],
+                  decoration:
+                      const InputDecoration(labelText: 'Telefon rodiče'),
+                  validator: (value) =>
+                      _validators['telefonRodice']?.validator(value),
+                ),
+              ],
+            ),
+            TextFormField(
+              controller: _controllers['poznamka'],
+              decoration: const InputDecoration(
+                  labelText: 'Poznámka',
+                  border: OutlineInputBorder(),
+                  hintText: 'Tento text se nebude tisknout'),
+              maxLines: 3,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  MemoryOsoba osoba = MemoryOsoba.fullNamed(
+                    id: null,
+                    jmeno: _controllers['jmeno']!.text,
+                    prijmeni: _controllers['prijmeni']!.text,
+                    datumNarozeni: DateFormat('dd.MM.yyyy')
+                        .parse(_controllers['datumNarozeni']!.text),
+                    adresa: _controllers['adresa']!.text,
+                    cisloPojisteni: _controllers['cisloPojisteni']!.text,
+                    jmenoRodice: _controllers['jmenoRodice']!.text,
+                    telefonRodice: _controllers['telefonRodice']!.text,
+                    emailRodice: _controllers['emailRodice']!.text,
+                    zdravotniPojistovna:
+                        _controllers['zdravotniPojistovna']!.text,
+                    poznamka: _controllers['poznamka']!.text,
+                    pohlavi: int.parse(_controllers['pohlavi']!.text),
+                    zpusobilost: false,
+                    bezinfekcnost: false,
+                    wasPrinted: false,
+                    oddil: '',
+                    prisel: false,
+                    potvrzeniPath: '',
+                  );
+                  _showPersonDetails(osoba);
+                }
+              },
+              child: const Text('Odeslat'),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
