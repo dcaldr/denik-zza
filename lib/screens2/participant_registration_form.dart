aa41351c7e75c995144e96aa38178fa9c427e31f
Dan Caldr <cald01@vse.cz>
2024-07-28 16:01:03 +0200

    ref: participant_registration_form.dart

--- lib/screens2/participant_registration_form.dart
+++ lib/screens2/participant_registration_form.dart
@@ -3,7 +3,6 @@ import 'package:denik_zza/screens2/widgets/custom_date_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:denik_zza/database/in_memory_structures_tmp/memory_osoba.dart';
-
 import '../database/database_wrapper.dart';
 import '../input/input_hold.dart';
 import '../input/rodne_cislo.dart';
@@ -14,12 +13,10 @@ class ParticipantRegistrationForm extends StatefulWidget {
   const ParticipantRegistrationForm({super.key, this.osoba});
 
   @override
-  _ParticipantRegistrationFormState createState() =>
-      _ParticipantRegistrationFormState();
+  _ParticipantRegistrationFormState createState() => _ParticipantRegistrationFormState();
 }
 
-class _ParticipantRegistrationFormState
-    extends State<ParticipantRegistrationForm> {
+class _ParticipantRegistrationFormState extends State<ParticipantRegistrationForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final Map<String, TextEditingController> _controllers = {
     'jmeno': TextEditingController(),
@@ -41,7 +38,7 @@ class _ParticipantRegistrationFormState
     'datumNarozeni': DatumNarozeniHold(),
     'adresa': AdresaHold(),
     'cisloPojisteni': CisloPojisteniHold(),
-    'jmenoRodice': JmenoHold( columnName: 'jmeno rodiče'),
+    'jmenoRodice': JmenoHold(columnName: 'jmeno rodiče'),
     'telefonRodice': TelefonHold(),
     'emailRodice': EmailHold(),
     'zdravotniPojistovna': PojistovnaHold(),
@@ -50,29 +47,33 @@ class _ParticipantRegistrationFormState
   };
 
   @override
-void initState() {
-  super.initState();
-  if (widget.osoba != null) {
-    _controllers['jmeno']!.text = widget.osoba!.jmeno;
-    _controllers['prijmeni']!.text = widget.osoba!.prijmeni;
-    _controllers['cisloPojisteni']!.text = widget.osoba!.cisloPojisteni!;
-    if (widget.osoba!.datumNarozeni != null) {
-      _controllers['datumNarozeni']!.text = DateFormat('dd.MM.yyyy').format(widget.osoba!.datumNarozeni!);
+  void initState() {
+    super.initState();
+    if (widget.osoba != null) {
+      _populateFields(widget.osoba!);
     }
-    _controllers['pohlavi']!.text = widget.osoba!.pohlavi.toString();
-    _controllers['zdravotniPojistovna']!.text = widget.osoba!.zdravotniPojistovna!;
-    _controllers['adresa']!.text = widget.osoba!.adresa!;
-    _controllers['jmenoRodice']!.text = widget.osoba!.jmenoRodice!;
-    _controllers['emailRodice']!.text = widget.osoba!.emailRodice!;
-    _controllers['telefonRodice']!.text = widget.osoba!.telefonRodice!;
-    _controllers['poznamka']!.text = widget.osoba!.poznamka!;
-  }
-  _controllers['cisloPojisteni']!.addListener(() {
-    setState(() {
-      guessAndFillFields(_controllers['cisloPojisteni']!.text);
+    _controllers['cisloPojisteni']!.addListener(() {
+      setState(() {
+        guessAndFillFields(_controllers['cisloPojisteni']!.text);
+      });
     });
-  });
-}
+  }
+
+  void _populateFields(MemoryOsoba osoba) {
+    _controllers['jmeno']!.text = osoba.jmeno;
+    _controllers['prijmeni']!.text = osoba.prijmeni;
+    _controllers['cisloPojisteni']!.text = osoba.cisloPojisteni!;
+    if (osoba.datumNarozeni != null) {
+      _controllers['datumNarozeni']!.text = DateFormat('dd.MM.yyyy').format(osoba.datumNarozeni!);
+    }
+    _controllers['pohlavi']!.text = osoba.pohlavi.toString();
+    _controllers['zdravotniPojistovna']!.text = osoba.zdravotniPojistovna!;
+    _controllers['adresa']!.text = osoba.adresa!;
+    _controllers['jmenoRodice']!.text = osoba.jmenoRodice!;
+    _controllers['emailRodice']!.text = osoba.emailRodice!;
+    _controllers['telefonRodice']!.text = osoba.telefonRodice!;
+    _controllers['poznamka']!.text = osoba.poznamka!;
+  }
 
   @override
   void dispose() {
@@ -82,47 +83,44 @@ void initState() {
 
   void _submitForm() async {
     if (_formKey.currentState!.validate()) {
-      MemoryOsoba osoba = MemoryOsoba.fullNamed(
-        id: widget.osoba?.id,
-        jmeno: _controllers['jmeno']!.text,
-        prijmeni: _controllers['prijmeni']!.text,
-        datumNarozeni: DateFormat('dd.MM.yyyy').parse(_controllers['datumNarozeni']!.text),
-        adresa: _controllers['adresa']!.text,
-        cisloPojisteni: _controllers['cisloPojisteni']!.text,
-        jmenoRodice: _controllers['jmenoRodice']!.text,
-        telefonRodice: _controllers['telefonRodice']!.text,
-        emailRodice: _controllers['emailRodice']!.text,
-        zdravotniPojistovna: _controllers['zdravotniPojistovna']!.text,
-        poznamka: _controllers['poznamka']!.text,
-        pohlavi: int.parse(_controllers['pohlavi']!.text),
-        zpusobilost: false,
-        bezinfekcnost: false,
-        wasPrinted: false,
-        oddil: '',
-        prisel: false,
-        potvrzeniPath: '',
-      );
-
+      MemoryOsoba osoba = _createMemoryOsoba();
       if (widget.osoba == null) {
         bool insertSuccess = await DatabaseWrapper.getDatabase().addOsoba(osoba);
-        if (!insertSuccess) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Insert failed')),
-          );
-        } else {
-          _showPersonDetails(osoba);
-        }
+        _showSnackBar(insertSuccess ? 'Insert successful' : 'Insert failed');
+        if (insertSuccess) _showPersonDetails(osoba);
       } else {
         int updateResult = await DatabaseWrapper.getDatabase().updateParticipant(osoba.id, osoba);
-        if (updateResult == 0) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Update failed')),
-          );
-        } else {
-          _showPersonDetails(osoba);
-        }
+        _showSnackBar(updateResult > 0 ? 'Update successful' : 'Update failed');
+        if (updateResult > 0) _showPersonDetails(osoba);
       }
     }
+  }
+
+  MemoryOsoba _createMemoryOsoba() {
+    return MemoryOsoba.fullNamed(
+      id: widget.osoba?.id,
+      jmeno: _controllers['jmeno']!.text,
+      prijmeni: _controllers['prijmeni']!.text,
+      datumNarozeni: DateFormat('dd.MM.yyyy').parse(_controllers['datumNarozeni']!.text),
+      adresa: _controllers['adresa']!.text,
+      cisloPojisteni: _controllers['cisloPojisteni']!.text,
+      jmenoRodice: _controllers['jmenoRodice']!.text,
+      telefonRodice: _controllers['telefonRodice']!.text,
+      emailRodice: _controllers['emailRodice']!.text,
+      zdravotniPojistovna: _controllers['zdravotniPojistovna']!.text,
+      poznamka: _controllers['poznamka']!.text,
+      pohlavi: int.parse(_controllers['pohlavi']!.text),
+      zpusobilost: false,
+      bezinfekcnost: false,
+      wasPrinted: false,
+      oddil: '',
+      prisel: false,
+      potvrzeniPath: '',
+    );
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
   }
 
   void guessAndFillFields(String inText) {
@@ -130,20 +128,16 @@ void initState() {
       RodneCislo rc = RodneCislo(inText);
       DateTime datumNarozeni = rc.getDatumNarozeni();
       int pohlavi = rc.getPohlavi();
-
-      if (_controllers['datumNarozeni'] != null &&
-          _controllers['datumNarozeni']!.text.isEmpty) {
-        _controllers['datumNarozeni']!.text =
-            DateFormat('dd.MM.yyyy').format(datumNarozeni);
+      //TODO: add animation for highlighting the autofilled fields !!
+      if (_controllers['datumNarozeni']!.text.isEmpty) {
+        _controllers['datumNarozeni']!.text = DateFormat('dd.MM.yyyy').format(datumNarozeni);
       }
-
-      if (_controllers['pohlavi'] != null &&
-          _controllers['pohlavi']!.text.isEmpty) {
+      if (_controllers['pohlavi']!.text.isEmpty) {
         _controllers['pohlavi']!.text = pohlavi.toString();
       }
     }
   }
-/// for debug purposes
+
   void _showPersonDetails(MemoryOsoba osoba) {
     showDialog(
       context: context,
@@ -166,127 +160,65 @@ void initState() {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(title: const Text('Registrace Účastníka')),
-    drawer: const AppDrawer(),
-    body: Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            GridView.count(
-              crossAxisCount: 3,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
-              childAspectRatio: 3 / 1,
-              children: [
-                TextFormField(
-                  controller: _controllers['jmeno'],
-                  decoration: const InputDecoration(labelText: 'Jméno'),
-                  validator: (value) {
-                    final validator = _validators['jmeno']?.validator(value);
-                    if (value == null || value.isEmpty) {
-                      return 'Jméno je povinné pole';
-                    }
-                    return validator;
-                  },
-                ),
-                TextFormField(
-                  controller: _controllers['prijmeni'],
-                  decoration: const InputDecoration(labelText: 'Příjmení'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Příjmení je povinné pole';
-                    }
-                    return _validators['prijmeni']?.validator(value);
-                  },
-                ),
-                TextFormField(
-                  controller: _controllers['cisloPojisteni'],
-                  decoration:
-                      const InputDecoration(labelText: 'Číslo Pojištěnce'),
-                  validator: (value) =>
-                      _validators['cisloPojisteni']?.validator(value),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: CustomDatePicker(
-                        controller: _controllers['datumNarozeni']!,
-                        labelText: 'Datum Narození',
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: TextFormField(
-                        controller: _controllers['pohlavi'],
-                        decoration: const InputDecoration(labelText: 'Pohlaví'),
-                        validator: (value) =>
-                            _validators['pohlavi']?.validator(value),
-                      ),
-                    ),
-                  ],
-                ),
-                TextFormField(
-                  controller: _controllers['zdravotniPojistovna'],
-                  decoration: const InputDecoration(
-                      labelText: 'Zdravotní Pojišťovna'),
-                  validator: (value) =>
-                      _validators['zdravotniPojistovna']?.validator(value),
-                ),
-                TextFormField(
-                  controller: _controllers['adresa'],
-                  decoration: const InputDecoration(labelText: 'Adresa'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Adresa je povinné pole';
-                    }
-                    return _validators['adresa']?.validator(value);
-                  },
-                ),
-                TextFormField(
-                  controller: _controllers['jmenoRodice'],
-                  decoration:
-                      const InputDecoration(labelText: 'Jméno rodiče'),
-                  validator: (value) =>
-                      _validators['jmenoRodice']?.validator(value),
-                ),
-                TextFormField(
-                  controller: _controllers['emailRodice'],
-                  decoration:
-                      const InputDecoration(labelText: 'Email rodiče'),
-                  validator: (value) =>
-                      _validators['emailRodice']?.validator(value),
-                ),
-                TextFormField(
-                  controller: _controllers['telefonRodice'],
-                  decoration:
-                      const InputDecoration(labelText: 'Telefon rodiče'),
-                  validator: (value) =>
-                      _validators['telefonRodice']?.validator(value),
-                ),
-              ],
-            ),
-            TextFormField(
-              controller: _controllers['poznamka'],
-              decoration: const InputDecoration(
-                  labelText: 'Poznámka',
-                  border: OutlineInputBorder(),
-                  hintText: 'Tento text se nebude tisknout'),
-              maxLines: 3,
-            ),
-        ElevatedButton(
-          onPressed: _submitForm,
-          child: Text(widget.osoba == null ? 'Přidat' : 'Aktualizovat'),
-        ),
-          ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registrace Účastníka')),
+      drawer: const AppDrawer(),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildGridView(),
+              _buildTextField('poznamka', 'Poznámka', 'Tento text se nebude tisknout', maxLines: 3),
+              ElevatedButton(onPressed: _submitForm, child: Text(widget.osoba == null ? 'Přidat' : 'Aktualizovat')),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
+
+  Widget _buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+      childAspectRatio: 3 / 1,
+      children: [
+        _buildTextField('jmeno', 'Jméno', 'Jméno je povinné pole'),
+        _buildTextField('prijmeni', 'Příjmení', 'Příjmení je povinné pole'),
+        _buildTextField('cisloPojisteni', 'Číslo Pojištěnce', null),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: CustomDatePicker(controller: _controllers['datumNarozeni']!, labelText: 'Datum Narození')),
+            const SizedBox(width: 8.0),
+            Expanded(child: _buildTextField('pohlavi', 'Pohlaví', null)),
+          ],
+        ),
+        _buildTextField('zdravotniPojistovna', 'Zdravotní Pojišťovna', null),
+        _buildTextField('adresa', 'Adresa', 'Adresa je povinné pole'),
+        _buildTextField('jmenoRodice', 'Jméno rodiče', null),
+        _buildTextField('emailRodice', 'Email rodiče', null),
+        _buildTextField('telefonRodice', 'Telefon rodiče', null),
+      ],
+    );
+  }
+
+  Widget _buildTextField(String key, String labelText, String? validatorText, {int maxLines = 1}) {
+    return TextFormField(
+      controller: _controllers[key],
+      decoration: InputDecoration(labelText: labelText, border: maxLines > 1 ? OutlineInputBorder() : null),
+      validator: (value) {
+        if (validatorText != null && (value == null || value.isEmpty)) {
+          return validatorText;
+        }
+        return _validators[key]?.validator(value);
+      },
+      maxLines: maxLines,
+    );
+  }
 }
