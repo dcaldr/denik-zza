ac8f7686eabf388f670b6693366c5f4e9e17ef80
Dan Caldr <cald01@vse.cz>
2024-07-28 18:01:37 +0200

    feat: file_viewer_screen_widget.dart

---
+++ lib/screens2/widgets/file_viewer_screen_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:printing/printing.dart';
+import 'dart:io';
+
+class FileViewerScreen extends StatefulWidget {
+  final String initialFilePath;
+
+  const FileViewerScreen({super.key, required this.initialFilePath});
+
+  @override
+  _FileViewerScreenState createState() => _FileViewerScreenState();
+}
+
+class _FileViewerScreenState extends State<FileViewerScreen> {
+  late String filePath;
+
+  @override
+  void initState() {
+    super.initState();
+    filePath = widget.initialFilePath;
+  }
+
+  void updateFilePath(String newFilePath) {
+    setState(() {
+      filePath = newFilePath;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String extension = path.extension(filePath).toLowerCase();
+
+    return Scaffold(
+      body: _buildView(extension),
+    );
+  }
+
+  Widget _buildView(String extension) {
+    if (extension == '.pdf') {
+      return _buildPDFView();
+    } else if (extension == '.jpg' || extension == '.jpeg' || extension == '.png') {
+      return _buildImageView();
+    } else {
+      return const Center(child: Text('Unsupported file type'));
+    }
+  }
+
+  Widget _buildPDFView() {
+    return PdfPreview(
+      build: (format) => File(filePath).readAsBytesSync(),
+    );
+  }
+
+  Widget _buildImageView() {
+    return InteractiveViewer(
+      minScale: 0.2,
+      maxScale: 10,
+      child: Image.file(
+        File(filePath),
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
