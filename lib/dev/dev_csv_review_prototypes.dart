fc5db2b321412108da083b29f796325397d77d18
Dan Caldr <cald01@vse.cz>
2025-10-25 21:00:08 +0200

    ref: removed old code

--- lib/dev/dev_csv_review_prototypes.dart
+++
@@ -1,4 +0,0 @@
-void main() {
-  throw StateError(
-      'Prototype switcher removed. Run dev_csv_review_table.dart instead.');
-}
--- lib/dev/dev_csv_review_split.dart
+++
@@ -1,3 +0,0 @@
-void main() {
-  throw StateError('Prototype removed. Run dev_csv_review_table.dart instead.');
-}
